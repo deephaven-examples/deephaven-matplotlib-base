bf115bba464dbb25c101a44882fb4913aba68d0e
Amanda L Martin <hythloda@gmail.com>
2022-03-07 15:13:04 -0500

    examples

---
+++ data/notebooks/5example_tri_surf.py
@@ -0,0 +1,14 @@
+plt.figure(figsize=(8, 8))
+ax = plt.axes(projection="3d")
+x = df['peak-rpm']
+y = df['city-mpg']
+z = z_function(x, y)
+ax.plot_trisurf(x, y, z,
+                cmap='viridis', edgecolor='none');
+ax.set_xlabel("Peak RPM")
+ax.set_ylabel("City-MPG")
+ax.set_title("Peak RPM vs City-MPG")
+ax.view_init(60, 25)
+plt.show()
+
+tri_surf=plt.gcf()
